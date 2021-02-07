54af0fdf8fd2b32a1bfa8056b0e772aeddaa6f27
Ettore Galli <ettore.galli@gmail.com>
2021-02-07 15:27:30 +0100

    Project starting point

---
+++ synth-core.js
@@ -0,0 +1,104 @@
+'use strict';
+
+class WebSynth {
+
+    constructor(
+        window
+    ) {
+        console.log("AudioPage init init...")
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+        this.audioContext = new AudioContext();
+
+        this.oscillator = this.audioContext.createOscillator();
+        this.oscillator.type = "square";
+
+        this.filter = this.audioContext.createBiquadFilter();
+        this.filter.type = 'lowpass';
+        this.filter.frequency.value = 0;
+        this.filter.Q.value = 4;
+
+        this.gainNode = this.audioContext.createGain();
+        this.gainNode.gain.value = 0;
+
+        this.oscillator
+            .connect(this.filter)
+            .connect(this.gainNode)
+            .connect(this.audioContext.destination);
+        this.oscillator.start(0);
+
+        const thisClassContext = this;
+
+        this.resumeIfSuspended = () => {
+            if (this.audioContext.state === 'suspended') {
+                this.audioContext.resume();
+            }
+        }
+
+        this.setFrequency = (value) => {
+            this.resumeIfSuspended();
+            this.oscillator.frequency.value = value;
+        }
+
+        this.setVolume = (value) => {
+            this.resumeIfSuspended();
+            this.gainNode.gain.value = value;
+        }
+
+        this.setVolumeWithAttackTime = (volumeValue, attackTime) => {
+            this.resumeIfSuspended();
+            
+            this.gainNode.gain.linearRampToValueAtTime(
+                volumeValue,
+                this.audioContext.currentTime + attackTime
+            );
+        }
+
+        this.setFilterFrequency = (value) => {
+            this.resumeIfSuspended();
+            this.filter.frequency.value = value;
+        }
+
+        this.setFilterQ = (value) => {
+            this.resumeIfSuspended();
+            this.filter.Q.value = value;
+        }
+
+        this.startNote = (frequency, volume, attackTime) => {
+            this.setFrequency(frequency);
+            this.setVolumeWithAttackTime(volume, attackTime);
+        }
+
+        this.playNote = (frequency, volume) => {
+            this.resumeIfSuspended();
+            this.startNote(frequency, parseFloat(volume), 0.1);
+        }
+
+        this.releaseNote = () => {
+            this.gainNode.gain.cancelScheduledValues(this.audioContext.currentTime);
+            this.setVolume(0);
+        }
+
+
+        this.chromaticNoteMap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(
+            (n) => 220 * Math.pow(2, n / 12)
+        );
+        
+        this.pentathonicNoteMap = [0, 2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 26].map(
+            (n) => 220 * Math.pow(2, n / 12)
+        );
+
+        this.noteMap = this.pentathonicNoteMap;
+
+
+        this.playNoteNumber = (n, volume) => {
+            const frequency = this.noteMap[n - 1];
+            this.playNote(frequency, parseFloat(volume));
+        }
+
+        console.log("AudioPage init completed.")
+
+    }
+
+}
+
---
+++ synth-ui.js
@@ -0,0 +1,130 @@
+'use strict'
+
+
+class SynthUIState {
+    constructor() {
+
+        this.state = {
+            volume: 0,
+            filterFrequency: 0,
+            filterQ: 0
+        };
+
+        this.updateSubscribers = [];
+
+        this.addUppdateSubscriber = function (subscriber) {
+            this.updateSubscribers.push(subscriber);
+        }
+
+        this.getState = () => {
+            return { ...this.state };
+        }
+
+        this.updateState = function (key, value) {
+            this.state = {
+                ...this.state,
+                [key]: value
+            };
+            this.updateSubscribers.forEach(subscriber => subscriber(this.getState()));
+        }
+
+
+        this.getVolume = () => this.getState().volume;
+        this.getFilterFrequency = () => this.getState().filterFrequency;
+        this.getFilterQ = () => this.getState().filterQ;
+    }
+
+}
+
+
+
+
+
+function getKeyIdFromNumber(keyNumber) {
+    return "area" + String(keyNumber);
+}
+
+function getKeyFromNumber(keyNumber) {
+    return document.getElementById(getKeyIdFromNumber(keyNumber))
+}
+
+function buildPadKeyboard(synth, synthState, numberOfKeys, keyAreaId) {
+    for (let i = 0; i < numberOfKeys; i++) {
+
+        const keyNumber = i + 1;
+        const key = document.getElementById("key-template").content.cloneNode(true).children[0];
+
+        key.id = getKeyIdFromNumber(keyNumber);
+        key.style.width = String(95.0 / numberOfKeys) + "%";
+        document.getElementById(keyAreaId).appendChild(key);
+
+
+        key.onmouseover = function (e) {
+            synth.playNoteNumber(keyNumber, synthState.getVolume());
+        }
+
+        key.onmousemove = function (e) {
+            var rect = e.target.getBoundingClientRect();
+            var x = e.clientX - rect.left; // x position within the element.
+            var y = e.clientY - rect.top;  // y position within the element.
+            console.log("flt freq " + synthState.getFilterFrequency())
+            var filterFrequency = (y / rect.height) * synthState.getFilterFrequency();
+            synth.setFilterFrequency(filterFrequency);
+        }
+
+        key.onmouseleave = function (e) {
+            synth.releaseNote();
+        }
+
+    }
+}
+
+function initSynth(window, document) {
+
+    const synth = new WebSynth(window);
+    const synthUIState = new SynthUIState();
+
+    const volumeController = document.getElementById('volume');
+    const filterFrequency = document.getElementById('filter-frequency');
+    const filterQ = document.getElementById('filter-q');
+
+    synthUIState.addUppdateSubscriber(
+        (state) => {
+            synth.setVolume(state.volume);
+        }
+    );
+
+    synthUIState.addUppdateSubscriber(
+        (state) => {
+            synth.setFilterFrequency(state.filterFrequency);
+        }
+    );
+
+    synthUIState.addUppdateSubscriber(
+        (state) => {
+            synth.setFilterQ(state.filterQ);
+        }
+    );
+
+    volumeController.addEventListener('input', function () {
+        synthUIState.updateState("volume", this.value);
+    }
+    );
+
+    filterFrequency.addEventListener('input', function () {
+        synthUIState.updateState("filterFrequency", this.value);
+    }
+    );
+
+    filterQ.addEventListener('input', function () {
+        synthUIState.updateState("filterQ", this.value);
+    }
+    );
+
+    const numberOfKeys = synth.noteMap.length;
+
+    buildPadKeyboard(synth, synthUIState, numberOfKeys, "key-area-up");
+    buildPadKeyboard(synth, synthUIState, numberOfKeys, "key-area-down");
+
+    window.ettore = synthUIState; // DEBUG-ONLY
+}
